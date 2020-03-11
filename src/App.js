4e51140ff905b2398a7c7f64b1c014b04eda3d04
iandstevens92 <iandstevens92@gmail.com>
2020-03-11 19:51:29 +0200

    screw you mandrew

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 style={{ borderBottom: "1px solid black", paddingBottom: "20px", marginBottom: 0 }}>Chess Score</h1>
+			<div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+				<div style={{ flex: 1 }}>
+					<h2>Mandrew</h2>
+					<p>0</p>
+				</div>
+				<div style={{ flex: 1, borderLeft: "1px solid black" }}>
+					<h2>Ian</h2>
+					<p>1</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
